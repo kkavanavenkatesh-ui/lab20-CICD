d922d859b094a77ab3d0c77ddfba0eaab667011a
kkavanavenkatesh-ui <kkavanavenkatesh@gmail.com>
2026-04-23 08:59:24 +0530

    Add multiply method to Calculator class

--- src/main/java/com/devops/lab/Calculator.java
+++ src/main/java/com/devops/lab/Calculator.java
@@ -1,9 +1,13 @@
 package com.devops.lab;
 
 public class Calculator {
-    // Methods should be INSIDE these braces
+    
     public int add(int a, int b) {
         return a + b;
     }
-} 
-// Nothing should be out here!
+
+    // Add this method!
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
